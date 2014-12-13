e2205676c7f3463668d006f1d7dbafffef8740b1
sebastien_andreu <deadyman@free.fr>
2014-12-13 04:37:53 +0000

    Toolbar shadow

--- src/toolbar.jsx
+++ src/toolbar.jsx
@@ -6,10 +6,19 @@ var React = require('react');
 
 var Toolbar = React.createClass({
   
+    propTypes: {
+        zDepth: React.PropTypes.number,
+    },
+    
+    getDefaultProps: function() {
+        return {
+            zDepth: 0,
+        };
+    },
+    
     render: function() {
-        var classNames = 'md-whiteframe-z' + this.props.zDepth;
         return (
-            React.createElement('md-toolbar', {className: 'md-default-theme'}, this.props.children)
+            React.createElement('md-toolbar', {className:  + 'md-default-theme md-whiteframe-z' + this.props.zDepth}, this.props.children)
         );
     }
 });
