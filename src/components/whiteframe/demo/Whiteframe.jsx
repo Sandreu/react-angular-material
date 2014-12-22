5426a783ae6b80a8c76a8bdd3d7bd844afc433bf
sebastien_andreu <deadyman@free.fr>
2014-12-22 20:00:15 +0000

    Whiteframe Demo

---
+++ src/components/whiteframe/demo/Whiteframe.jsx
@@ -0,0 +1,27 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react'),
+    Md = require('react-md');
+    
+var WhiteframeDemo = React.createClass({
+    render: function () {
+        var demos = [];
+        
+        for (var i=1 ; i<=5 ; ++i) {
+            demos.push(
+                <Md.Whiteframe zDepth={i} style={{margin: '0 50px 30px 50px', padding: '20px'}}>
+                    <p>zDepth={'{' + i + '}'}</p>
+                </Md.Whiteframe>
+            );
+        }
+        return (
+            <div>
+                {demos}
+            </div>
+        );
+    }
+});
+
+module.exports = WhiteframeDemo;
