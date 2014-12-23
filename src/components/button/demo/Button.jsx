8e48fdb59eb34d651e7abb661271495ef84ce508
sebastien_andreu <deadyman@free.fr>
2014-12-23 15:02:18 +0000

    [Demo] Use divider component

--- src/components/button/demo/Button.jsx
+++ src/components/button/demo/Button.jsx
@@ -9,13 +9,14 @@ var ButtonDemo = React.createClass({
     
     render: function () {
         return (
-            <div>
+            <div layout="column" layoutAlign="space-around">
                 <div layout="row" layoutAlign="center center" className="demo-well">
                     <Md.Button>Button</Md.Button>
                     <Md.Button primary={true}>Primary</Md.Button>
                     <Md.Button disabled={true}>Disabled</Md.Button>
                     <Md.Button warn={true}>Warn</Md.Button>
                 </div>
+                <Md.Divider />
                 <div layout="row" layoutAlign="center center" className="demo-well">
                     <span className="demo-well-title">decorator="raised"</span>
                     <Md.Button decorator="raised">Button</Md.Button>
@@ -23,6 +24,7 @@ var ButtonDemo = React.createClass({
                     <Md.Button decorator="raised" disabled={true}>Disabled</Md.Button>
                     <Md.Button decorator="raised" warn={true}>Warn</Md.Button>
                 </div>
+                <Md.Divider />
                 <div layout="row" layoutAlign="center center" className="demo-well">
                     <span className="demo-well-title">decorator="fab"</span>
                     <Md.Button decorator="fab">i</Md.Button>
